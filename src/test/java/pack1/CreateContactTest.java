92ec021ee33808b72da38a35b1255728edc78e9b
RahulJamkhandi <118538374+RahulJamkhandi@users.noreply.github.com>
2024-11-13 10:59:14 +0530

    added extra funtionalities

--- src/test/java/pack1/CreateContactTest.java
+++ src/test/java/pack1/CreateContactTest.java
@@ -7,10 +7,10 @@ public class CreateContactTest {
 	public void createContactTest() 
 	{
 		System.out.println("A test to create contact");
-		String URL = System.getProperty("url","localhost://8088");
-		String BROWSER = System.getProperty("browser","chrome");
-		String USERNAME = System.getProperty("username","admin");
-		String PASSWORD = System.getProperty("password","manager");
+		String URL = System.getProperty("url");
+		String BROWSER = System.getProperty("browser");
+		String USERNAME = System.getProperty("username");
+		String PASSWORD = System.getProperty("password");
 		
 		System.out.println(URL);
 		System.out.println(BROWSER);
@@ -21,5 +21,15 @@ public class CreateContactTest {
 	@Test
 	public void modifyContactTest() {
 		System.out.println("A test to modify contact");
+	}
+	
+	@Test
+	public void editContactTest() {
+		System.out.println("A test to edit contact");
+	}
+	
+	@Test
+	public void deleteContactTest() {
+		System.out.println("A test to delete contact");
 	}
 }
